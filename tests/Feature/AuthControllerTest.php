f37c57d2782abc4a7f56f1b53e557a21830e2805
betelhem demisse <betide2011@gmail.com>
2024-08-16 13:35:43 +0300

    1:18

--- tests/Feature/AuthControllerTest.php
+++ tests/Feature/AuthControllerTest.php
@@ -15,7 +15,7 @@ class AuthControllerTest extends TestCase
     {
         $response = $this->postJson('/api/register', [
             'name' => 'Test User',
-            'email' => 'test18@example.com',
+            'email' => 'test20@example.com',
             'password' => 'password123@',
             'password_confirmation' => 'password123@',
         ]);
@@ -36,7 +36,7 @@ class AuthControllerTest extends TestCase
         //     'password' => bcrypt('password123@'),
         // ]);
         $response = $this->postJson('/api/login', [
-            'email' => 'test18@example.com',
+            'email' => 'test20@example.com',
             'password' => bcrypt('password123@'),
         ]);
         $response->assertStatus(200);
